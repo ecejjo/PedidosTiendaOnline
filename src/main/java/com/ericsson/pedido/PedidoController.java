341323d0524b51fd9088cdb65fa3e9c17fded512
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-10-27 00:39:50 +0200

    Delete Pedido: working!!

--- src/main/java/com/ericsson/pedido/PedidoController.java
+++ src/main/java/com/ericsson/pedido/PedidoController.java
@@ -98,6 +98,7 @@ public class PedidoController {
 		else {
 			model.addAttribute("alertMessage", "Pedido no encontrado. [Id: " + id + "]");
 		}
+		model.addAttribute("pedidos", pedidosRepository.findAll());
 		return "TablonPedidos_template";
 	}
 
