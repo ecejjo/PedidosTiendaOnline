ff2ef5089fdab5a2a10d257dd0b9c791d3bb9f26
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-10-31 16:03:25 +0100

    NuevoPedido: refactored using jQuery

--- src/main/resources/static/TiendaOnline.js
+++ src/main/resources/static/TiendaOnline.js
@@ -6,23 +6,85 @@ var elementsList = new Array();
 var editable = false;
 
 function refreshSubmitButton() {
+	console.log("refreshSubmitButton(): starting ...")
 	if (document.getElementById("titulo-text-input").value == "") {
 		document.getElementById("submitFormButton").disabled = true;
 	}
 	else {
 		document.getElementById("submitFormButton").disabled = false;
 	}
+	console.log("refreshSubmitButton(): Done.")
 }
 
-function addElement(text) {
-	if (text != null) {
-		elementsList.push(text);		
+function refreshListaElementosText() {
+	console.log("refreshListaElementosText(): length is:" + $('ul#ListaElementos').children().length);
+	if ($('ul#ListaElementos').children().length == 0) {
+		$('p#ListaElementosText').text("No hay elementos en la lista.");		
 	}
 	else {
-		elementsList.push(document.getElementById("element-text-input").value);
-		document.getElementById("element-text-input").value = "";
+		$('p#ListaElementosText').text("Hay elementos");		
 	}
-	showElementsList();
+	console.log("refreshListaElementosText(): done.")
+}
+
+function addToListaElementos() {
+	
+	id = Math.random().toString(36).substring(2, 15);
+	// id = Date.now();
+	
+	aux = "<li id = " + id + ">";
+	aux += "<input type=text ";
+	aux += "value = '" + $('input#element-text-input').val() + "' ";
+	aux += ">";
+	aux += "<button style='display:none;' ";
+	aux += "onclick='deleteFromListaElementos(\"" + id + "\")'";
+	// aux += "onclick='deleteFromListaElementos($(this).parent().id)'";
+	// aux += "onclick=$(" + id + ").remove()";
+	aux += ">";
+	aux += "Borrar Elemento";
+	aux += "</button>";
+	aux += "</li>"
+		
+	$('ul#ListaElementos').append(aux);
+	$('input#element-text-input').val("");
+	
+	refreshListaElementosText();
+	refreshElementosFromList();
+	refreshDeleteButtonsInListaElementos();
+}
+
+function deleteFromListaElementos(id) {
+	console.log("id is: " + id);
+	$(id).remove("li");
+	// $(id).detach();
+    // $(this).parent().remove();
+	refreshDeleteButtonsInListaElementos();
+}
+
+function refreshElementosFromList() {
+
+	console.log("refreshElementosFromList(): running ...")
+	
+	var elementsArray = new Array();
+
+	$("ul#ListaElementos > li > input").each(function( index ) {
+		console.log("value is:" + index + ": " + $( this ).val() );
+		elementsArray.push($( this ).val());
+	});
+	
+	$('input#elementos').val(elementsArray);
+}
+
+function refreshDeleteButtonsInListaElementos() {
+	if ($('ul#ListaElementos').children().length > 1) {
+		// $('ul#ListaElementos > li > button').css("display", "block");
+		$('ul#ListaElementos > li > button').show();
+	}
+	else {
+		console.log("Hidding ...");
+		// $('ul#ListaElementos > li > button').hide();
+		$('ul#ListaElementos > li > button').css("display", "none");
+	}
 }
 
 function deleteElementFromList(index) {
@@ -134,4 +196,15 @@ function confirmarBorrarPedido(pedidoId) {
 function editarPedido(pedidoId) {
 	document.getElementById("editar-pedido-link").click();
 }
+
+
+//A $( document ).ready() block.
+$( document ).ready(function() {
+	
+    console.log( "Document ready!" );
+    $('input#titulo-text-input').blur(refreshSubmitButton);
+    $('button#add-element-button').click(addToListaElementos);
+    refreshListaElementosText();
+});
+
 
